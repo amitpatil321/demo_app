716817ee5b58efa4894025f32d2dc76f854fc8a1
Amit <amitpatil321@gmail.com>
2025-07-02 12:09:22 +0530

    feat: add primereact buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Button } from "primereact/button";
+import "primereact/resources/themes/bootstrap4-light-purple/theme.css";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h3>Hello world!</h3>
-      <p>This is first commit after jenkins setup</p>
+      <h2>{count}</h2>
+      <Button
+        label="Increment"
+        onClick={() => setCount((count) => count + 1)}
+      />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { PrimeReactProvider } from "primereact/api";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <PrimeReactProvider>
     <App />
-  </StrictMode>,
-)
+  </PrimeReactProvider>
+);
